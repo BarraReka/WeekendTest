23bdf347c56dd606302add5fc921cbdc1d90d564
rekabarra <rekabarra@gmail.com>
2017-02-25 14:21:04 +0200

    identify the max from n numbers

---
+++ src/MaxElemArray.java
@@ -0,0 +1,27 @@
+/**
+ * Created by blinstall on 2/25/2017.
+ */
+public class MaxElemArray {
+    public static void main(String[] args) {
+
+        int count = SkeletonJava.readIntConsole("how many elements?");
+        int[] elements = new int [count];
+
+       for(int i=0; i<count; i++){
+           elements[i]= SkeletonJava.readIntConsole("element?");
+       }
+
+        int max = findMax (elements);
+
+        SkeletonJava.printConsole(max);
+    }
+    public static int findMax(int[] elements){
+        int max = elements[0];
+        for (int i=1; i< elements.length; i++){
+            if(elements[i]>max){
+                max=elements[i];
+            }
+        }
+        return max;
+    }
+}
